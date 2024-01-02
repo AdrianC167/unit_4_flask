e2aaaa4218d38df7e0feb94c4e04ceaa7404e067
AdrianC167 <ac232126110@gmail.com>
2024-01-02 10:51:30 -0500

    added adding and deleting functions

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
@@ -7,7 +7,15 @@ my_todo = ["graduate", 'get into college']
 
 @app.route("/", methods= ["GET", 'POST'])
 def todo():
-    new_todo = request.form["new_todo"]
-    my_todo.append(new_todo)
+    if request.method == 'POST':
+        new_todo = request.form["new_todo"]
+        my_todo.append(new_todo)
+    
     return render_template("todo.html.jinja",todos = my_todo)
 
+
+@app.route('/delete_todo/<int:todo_index>', methods = ['POST'])
+def todo_delete(todo_index):
+    del my_todo[todo_index]
+    
+    return redirect('/')
