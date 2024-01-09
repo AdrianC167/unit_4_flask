80e10796ca15ab38cda931c263a53477df1459a8
AdrianC167 <ac232126110@gmail.com>
2024-01-09 10:46:29 -0500

    commit and push the cursors for adding and delete

--- todo.py
+++ todo.py
@@ -7,31 +7,37 @@ from pprint import pprint as print
 app = Flask(__name__)
 
 my_todo = ["graduate", 'get into college']
-
-@app.route("/", methods= ["GET", 'POST'])
-def todo():
-
-    conn = pymysql.connect(
+conn = pymysql.connect(
         database = "achen_todos",
         user = "achen",
         password = "232126110",
         host = "10.100.33.60",
         cursorclass = pymysql.cursors.DictCursor
+ )
 
-    )
+
+@app.route("/", methods= ["GET", 'POST'])
+def todo():
 
     cursor = conn.cursor()
-    cursor.execute("SELECT `description` FROM `todos`")
+    cursor.execute("SELECT * FROM `todos`")
     results = cursor.fetchall()
+    cursor.close()
+
     if request.method == 'POST':
-        new_todo = request.form[my_todo]
-        my_todo.append(new_todo)
+        new_todo = request.form["new_todo"]
+        cursor = conn.cursor()
+        cursor.execute(f"INSERT INTO `todos`(`description`) VALUES('{new_todo}')")         
+        cursor.close()
+        conn.commit()
     
     return render_template("todo.html.jinja",todos = results)
 
 
 @app.route('/delete_todo/<int:todo_index>', methods = ['POST'])
 def todo_delete(todo_index):
-    del my_todo[todo_index]
-    
+    cursor = conn.cursor()
+    cursor.execute(f"DELETE FROM `todos` WHERE `id` = {todo_index} ")
+    cursor.close()
+    conn.commit()
     return redirect('/')
